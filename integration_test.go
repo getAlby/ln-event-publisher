fb399e5089ac70246582f8e8c045a878abc5a3b9
kiwiidb <kwinten.debacker@hotmail.com>
2023-02-28 13:59:36 +0100

    write integration tests

---
+++ integration_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestLNEventPublisher(t *testing.T) {
+	// todo
+	// - mock LND
+	// - init PG
+	// - init Rabbit
+	// - add invoice
+	// - trigger paid invoice
+	// - check if invoice is published
+	// - restart service
+	// - check if invoice is restarted from right index
+	// - Add PG / Rabbit in CI to run tests on GH actions
+}
