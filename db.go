bf9c63599f19a16d1b2a3f072c93e95fa3915cac
kiwiidb <kwinten.debacker@hotmail.com>
2023-07-11 14:02:41 +0200

    db logger: error level

--- db.go
+++ db.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"os"
 	"time"
 
 	"github.com/lightningnetwork/lnd/lnrpc"
@@ -10,13 +12,20 @@ import (
 )
 
 func OpenDB(config *Config) (db *gorm.DB, err error) {
-	db, err = gorm.Open(postgres.Open(config.DatabaseUri), &gorm.Config{})
+	//overwrite logger so we don't print warnings for slow sql
+	//because we use db transactions that span the rabbitmq publish operation
+	dbLogger := logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+		SlowThreshold:             200 * time.Millisecond,
+		LogLevel:                  logger.Error,
+		IgnoreRecordNotFoundError: false,
+		Colorful:                  true,
+	})
+	db, err = gorm.Open(postgres.Open(config.DatabaseUri), &gorm.Config{
+		Logger: dbLogger,
+	})
 	if err != nil {
 		return nil, err
 	}
-	//don't print warnings for slow sql
-	//because we use db transactions that span the rabbitmq publish operation
-	db.Logger.LogMode(logger.Error)
 	sqlDB, err := db.DB()
 	if err != nil {
 		return nil, err
